d45ee38798811962f3a468b103632fd915116b49
ZUXING <53240486+ZUXING@users.noreply.github.com>
2020-07-15 01:18:36 +0800

    上传了第一个版本，更新了Readme

---
+++ QQPrinter.py
@@ -0,0 +1,30 @@
+import os
+import win32api
+import win32event
+import win32process
+import win32con
+import win32print
+
+#Version 0.75
+#ChenGuanglin Software Studio & ZUXING
+print("Codename:QQPrinter")
+print("QQ打印机开始工作")
+
+from win32com.shell.shell import ShellExecuteEx
+from win32com.shell import shellcon
+
+recvFilePath = 'xc\\'
+while 1:
+	dirs = os.listdir(recvFilePath)
+	for file in dirs:
+		if os.path.splitext(file)[1] == ".docx":
+			print(file)
+			process_info = ShellExecuteEx(nShow=win32con.SW_SHOW,
+				fMask=shellcon.SEE_MASK_NOCLOSEPROCESS,
+				lpVerb='print',
+				lpFile=recvFilePath + file,
+				lpParameters='/d:"%s"' % win32print.GetDefaultPrinter ())
+			win32event.WaitForSingleObject(process_info['hProcess'], -1)
+			print("打印完成")
+			os.remove(recvFilePath + file)
+os.system("pause")
